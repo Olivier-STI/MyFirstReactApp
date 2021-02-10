af100e7f782be6f143d28a56470bbcc9c0df3b72
Olivier <olivier@pop-os.localdomain>
2021-02-10 17:00:11 +0400

    withe card ui without picture

--- workshopreact/src/Menu.js
+++ workshopreact/src/Menu.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 import data from "./data"; 
 
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
 var Pizza = data.Pizza;
 var Array = [];
 var AllData = {};
@@ -18,16 +33,29 @@ AllData.Pizza = Pizza;
 class App extends Component {
     render() {
 		return (
-            <p style={{color:"Black"}}>
-            {Array.map(item => (
-                <div>
-                <h2>{item.name}</h2>
-                <h4>{item.price}</h4>
-                <p>{item.ingredients}</p>
-                </div>
-            ))}
-		</p>
-        );
-    }
-} 
+            
+<Card className={makeStyles.root}>
+    {Array.map(item => (
+        <Typography>
+            <CardActionArea>
+                <CardMedia
+                className={makeStyles.media}
+                image={item.image}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                    {item.name}
+                </Typography>
+                <Typography gutterBottom variant="h6" component="h6">
+                    {item.price}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {item.ingredients}
+                </Typography>
+            </CardContent>
+        </CardActionArea>
+      </Typography>
+    ))}
+</Card>
+        )}}; 
 export default App;
